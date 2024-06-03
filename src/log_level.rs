cd537fe22a966c798140ccfa63c480bf1248215f
berlinen <1292393108@qq.com>
2024-06-03 10:00:48 +0800

    feat: log impl display trait

--- src/log_level.rs
+++ src/log_level.rs
@@ -1,3 +1,22 @@
-use std::fmt::Display;
+use std::{default, fmt::Display};
 
 use clap::ValueEnum;
+
+#[derive(Debug, Default, PartialEq, PartialOrd, Eq, Ord, Clone, ValueEnum)]
+pub enum LogLevel {
+    Quiet,
+    Error,
+    #[default]
+    #[value(alias("all"))]
+    Info,
+}
+
+impl Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LogLevel::Quiet => write!(f, "quiet"),
+            LogLevel::Error => write!(f, "error"),
+            LogLevel::Info => write!(f, "info"),
+        }
+    }
+}
