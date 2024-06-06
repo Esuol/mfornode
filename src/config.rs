f280b84e4caf82b5d6ae74c8b7d3ac0e20e232db
berlinen <1292393108@qq.com>
2024-06-06 13:42:38 +0800

    feat: config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,6 @@
 use crate::arch::Arch;
 use crate::log_level::LogLevel;
 use crate::path_ext::PathExt;
+use crate::version_file_strategy::VersionFileStrategy;
+use dirs::{data_dir, home_dir};
+use url::Url;
