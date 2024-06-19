5872966049f7fe3e75ad89f0b2f734fe6ff21462
berlinen <1292393108@qq.com>
2024-06-19 14:37:03 +0800

    feat: installed_version fn list

--- src/installed_version.rs
+++ src/installed_version.rs
@@ -1,11 +1,29 @@
 use crate::version::Version;
+use anyhow::{Error, Ok};
 use std::path::Path;
-use anyhow::Error;
 use thiserror::Error;
 
 pub fn list<P: AsRef<Path>>(installations_dir: P) -> Result<Vec<Version>, Error> {
-  let mut vec = vec![];
-  for result_entry in installations_dir.as_ref().read_dir()? {
-    
-  }
+    let mut vec = vec![];
+    for result_entry in installations_dir.as_ref().read_dir()? {
+        let entry = result_entry?;
+        if entry
+            .file_name()
+            .to_str()
+            .map_or(false, |s| s.starts_with('.'))
+        {
+            continue;
+        }
+        let path = entry.path();
+        let filename = path
+            .file_name()
+            .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::NotFound))?
+            .to_str()
+            .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::NotFound))?;
+
+        let version = Version::parse(filename)?;
+        vec.push(version);
+
+        Ok(vec)
+    }
 }
