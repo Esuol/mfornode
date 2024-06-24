01c036779b34e1bbcd09726e135e1dea53736f75
berlinen <1292393108@qq.com>
2024-06-24 10:08:42 +0800

    feat: define Trait Extract

--- src/archive/extract.rs
+++ src/archive/extract.rs
@@ -37,3 +37,7 @@ impl From<crate::http::Error> for Error {
         Self::HttpError(err)
     }
 }
+
+pub trait Extract {
+    fn extract_into<P: AsRef<Path>>(self, path: P) -> Result<(), Error>;
+}
