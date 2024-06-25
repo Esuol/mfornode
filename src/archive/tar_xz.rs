80736c52b2f94b3e6ce5b626812e8baaade8b847
berlinen <1292393108@qq.com>
2024-06-25 14:53:50 +0800

    feat: zip

--- src/archive/tar_xz.rs
+++ src/archive/tar_xz.rs
@@ -11,3 +11,12 @@ impl<R: Read> TarXz<R> {
         Self { response }
     }
 }
+
+impl<R: Read> Extract for TarXz<R> {
+    fn extract_into<P: AsRef<Path>>(self, path: P) -> Result<(), Error> {
+        let xz_stream = xz2::read::XzDecoder::new(self.response);
+        let mut tar_archive = tar::Archive::new(xz_stream);
+        tar_archive.unpack(path)?;
+        Ok(())
+    }
+}
