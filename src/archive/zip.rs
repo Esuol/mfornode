90cd983a12c041444a559ce88bb4d38c53b4632a
berlinen <1292393108@qq.com>
2024-06-25 14:54:36 +0800

    feat: zip impl Extract for zip

--- src/archive/zip.rs
+++ src/archive/zip.rs
@@ -16,3 +16,66 @@ impl<R: Read> Zip<R> {
         Self { response }
     }
 }
+
+impl<R: Read> Extract for Zip<R> {
+    fn extract_into<P: AsRef<Path>>(mut self, path: P) -> Result<(), Error> {
+        let path = path.as_ref();
+        let mut tmp_zip_file = tempfile().expect("Can't get a temporary file");
+
+        debug!("Created a temporary zip file");
+        io::copy(&mut self.response, &mut tmp_zip_file)?;
+        debug!(
+            "Wrote zipfile successfully. Now extracting into {}.",
+            path.display()
+        );
+
+        let mut archive = ZipArchive::new(&mut tmp_zip_file)?;
+
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i)?;
+            let outpath = path.join(file.mangled_name());
+
+            {
+                let comment = file.comment();
+                if !comment.is_empty() {
+                    debug!("File {} comment: {}", i, comment);
+                }
+            }
+
+            if file.name().ends_with('/') {
+                debug!(
+                    "File {} extracted to \"{}\"",
+                    i,
+                    outpath.as_path().display()
+                );
+                fs::create_dir_all(&outpath)?;
+            } else {
+                debug!(
+                    "Extracting file {} to \"{}\" ({} bytes)",
+                    i,
+                    outpath.as_path().display(),
+                    file.size()
+                );
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p)?;
+                    }
+                }
+                let mut outfile = fs::File::create(&outpath)?;
+                io::copy(&mut file, &mut outfile)?;
+            }
+
+            // Get and Set permissions
+            #[cfg(unix)]
+            {
+                use std::os::unix::fs::PermissionsExt;
+
+                if let Some(mode) = file.unix_mode() {
+                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode))?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
