f70d8d4dbc7b5fe14894e393d340bccbf4e2886c
berlinen <1292393108@qq.com>
2024-06-26 10:28:13 +0800

    feat: define command impl

--- src/commands/command.rs
+++ src/commands/command.rs
@@ -1,4 +1,15 @@
-use crate::config::FnmConfig;
+use crate::config::{self, FnmConfig};
 use crate::outln;
 use colored::Colorize;
 
+pub trait Command: Sized {
+    type Error: std::error::Error;
+
+    fn apply(self, config: &FnmConfig) -> Result<(), Self::Error>;
+
+    fn handle_error(err: Self::Error, config: &FnmConfig) {
+        let err_s = format!("{err}");
+        outln!(config, Error, "{} {}", "error:".red().bold(), err_s.red());
+        std::process::exit(1);
+    }
+}
