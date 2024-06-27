62a956d326a6d778b0929594a9f6c86412e61c5d
berlinen <1292393108@qq.com>
2024-06-27 17:26:47 +0800

    feat: impl command completions mod

--- src/commands/completions.rs
+++ src/commands/completions.rs
@@ -5,3 +5,10 @@ use crate::{cli::Cli, shell::Shells};
 use clap::{CommandFactory, Parser, ValueEnum};
 use clap_complete::{Generator, Shell as ClapShell};
 use thiserror::Error;
+
+#[derive(Parser, Debug)]
+pub struct Completions {
+    /// The shell syntax to use. Infers when missing.
+    #[clap(long)]
+    shell: Option<Shells>,
+}
