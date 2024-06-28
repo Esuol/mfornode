06f0cd88bbb26eed7d691d9ea21a00efe7e05acc
berlinen <1292393108@qq.com>
2024-06-28 13:41:54 +0800

    feat: define env

--- src/commands/env.rs
+++ src/commands/env.rs
@@ -10,3 +10,27 @@ use colored::Colorize;
 use std::collections::HashMap;
 use std::fmt::Debug;
 use thiserror::Error;
+
+#[derive(clap::Parser, Debug, Default)]
+pub struct Env {
+    /// The shell syntax to use. Infers when missing.
+    #[clap(long)]
+    shell: Option<Shells>,
+    /// Print JSON instead of shell commands.
+    #[clap(long, conflicts_with = "shell")]
+    json: bool,
+    /// Deprecated. This is the default now.
+    #[clap(long, hide = true)]
+    multi: bool,
+    /// Print the script to change Node versions every directory change
+    #[clap(long)]
+    use_on_cd: bool,
+}
+
+fn generate_symlink_path() -> String {
+    format!(
+        "{}_{}",
+        std::process::id(),
+        chrono::Utc::now().timestamp_millis(),
+    )
+}
