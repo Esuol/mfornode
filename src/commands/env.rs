bf87e18c4f66e0622a7dc2864fa5fca3888ca97c
berlinen <1292393108@qq.com>
2024-06-28 13:42:19 +0800

    feat: impl commmand env

--- src/commands/env.rs
+++ src/commands/env.rs
@@ -34,3 +34,143 @@ fn generate_symlink_path() -> String {
         chrono::Utc::now().timestamp_millis(),
     )
 }
+
+fn make_symlink(config: &FnmConfig) -> Result<std::path::PathBuf, Error> {
+    let base_dir = directories::multishell_storage().ensure_exists_silently();
+    let mut temp_dir = base_dir.join(generate_symlink_path());
+
+    while temp_dir.exists() {
+        temp_dir = base_dir.join(generate_symlink_path());
+    }
+
+    match symlink_dir(config.default_version_dir(), &temp_dir) {
+        Ok(_) => Ok(temp_dir),
+        Err(source) => Err(Error::CantCreateSymlink { source, temp_dir }),
+    }
+}
+
+impl Command for Env {
+    type Error = Error;
+
+    fn apply(self, config: &FnmConfig) -> Result<(), Self::Error> {
+        if self.multi {
+            outln!(
+                config,
+                Error,
+                "{} {} is deprecated. This is now the default.",
+                "warning:".yellow().bold(),
+                "--multi".italic()
+            );
+        }
+
+        let multishell_path = make_symlink(config)?;
+        let multishell_path_str = multishell_path.to_str().unwrap().to_owned();
+
+        let binary_path = if cfg!(windows) {
+            multishell_path
+        } else {
+            multishell_path.join("bin")
+        };
+
+        let env_vars = HashMap::from([
+            ("FNM_MULTISHELL_PATH", multishell_path_str),
+            (
+                "FNM_VERSION_FILE_STRATEGY",
+                config.version_file_strategy().as_str().to_owned(),
+            ),
+            (
+                "FNM_DIR",
+                config.base_dir_with_default().to_str().unwrap().to_owned(),
+            ),
+            (
+                "FNM_LOGLEVEL",
+                <&'static str>::from(config.log_level().clone()).to_owned(),
+            ),
+            (
+                "FNM_NODE_DIST_MIRROR",
+                config.node_dist_mirror.as_str().to_owned(),
+            ),
+            (
+                "FNM_COREPACK_ENABLED",
+                config.corepack_enabled().to_string(),
+            ),
+            ("FNM_RESOLVE_ENGINES", config.resolve_engines().to_string()),
+            ("FNM_ARCH", config.arch.to_string()),
+        ]);
+
+        if self.json {
+            println!("{}", serde_json::to_string(&env_vars).unwrap());
+            return Ok(());
+        }
+
+        let shell: Box<dyn Shell> = self
+            .shell
+            .map(Into::into)
+            .or_else(infer_shell)
+            .ok_or(Error::CantInferShell)?;
+
+        println!("{}", shell.path(&binary_path)?);
+
+        for (name, value) in &env_vars {
+            println!("{}", shell.set_env_var(name, value));
+        }
+
+        if self.use_on_cd {
+            println!("{}", shell.use_on_cd(config)?);
+        }
+        if let Some(v) = shell.rehash() {
+            println!("{v}");
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(
+        "{}\n{}\n{}\n{}",
+        "Can't infer shell!",
+        "fnm can't infer your shell based on the process tree.",
+        "Maybe it is unsupported? we support the following shells:",
+        shells_as_string()
+    )]
+    CantInferShell,
+    #[error("Can't create the symlink for multishells at {temp_dir:?}. Maybe there are some issues with permissions for the directory? {source}")]
+    CantCreateSymlink {
+        #[source]
+        source: std::io::Error,
+        temp_dir: std::path::PathBuf,
+    },
+    #[error(transparent)]
+    ShellError {
+        #[from]
+        source: anyhow::Error,
+    },
+}
+
+fn shells_as_string() -> String {
+    Shells::value_variants()
+        .iter()
+        .map(|x| format!("* {x}"))
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_smoke() {
+        let config = FnmConfig::default();
+        Env {
+            #[cfg(windows)]
+            shell: Some(Shells::Cmd),
+            #[cfg(not(windows))]
+            shell: Some(Shells::Bash),
+            ..Default::default()
+        }
+        .call(config);
+    }
+}
