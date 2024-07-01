0d8b72db1c172997a262f90cf395935f3a58d889
berlinen <1292393108@qq.com>
2024-07-01 15:46:48 +0800

    feat: define exec Exec impl Command

--- src/commands/exec.rs
+++ src/commands/exec.rs
@@ -24,20 +24,87 @@ pub struct Exec {
 }
 
 impl Exec {
-  pub(crate) fn new_for_version(
-      version: &crate::version::Version,
-      cmd: &str,
-      arguments: &[&str],
-  ) -> Self {
-      let reader = UserVersionReader::Direct(UserVersion::Full(version.clone()));
-      let args: Vec<_> = std::iter::once(cmd)
-          .chain(arguments.iter().copied())
-          .map(String::from)
-          .collect();
-      Self {
-          version: Some(reader),
-          using_file: false,
-          arguments: args,
-      }
-  }
+    pub(crate) fn new_for_version(
+        version: &crate::version::Version,
+        cmd: &str,
+        arguments: &[&str],
+    ) -> Self {
+        let reader = UserVersionReader::Direct(UserVersion::Full(version.clone()));
+        let args: Vec<_> = std::iter::once(cmd)
+            .chain(arguments.iter().copied())
+            .map(String::from)
+            .collect();
+        Self {
+            version: Some(reader),
+            using_file: false,
+            arguments: args,
+        }
+    }
+}
+
+impl Cmd for Exec {
+    type Error = Error;
+
+    fn apply(self, config: &FnmConfig) -> Result<(), Self::Error> {
+        if self.using_file {
+            outln!(
+                config,
+                Error,
+                "{} {} is deprecated. This is now the default.",
+                "warning:".yellow().bold(),
+                "--using-file".italic()
+            );
+        }
+
+        let (binary, arguments) = self
+            .arguments
+            .split_first()
+            .ok_or(Error::NoBinaryProvided)?;
+
+        let version = self
+            .version
+            .unwrap_or_else(|| {
+                let current_dir = std::env::current_dir().unwrap();
+                UserVersionReader::Path(current_dir)
+            })
+            .into_user_version(config)
+            .ok_or(Error::CantInferVersion)?;
+
+        let applicable_version = choose_version_for_user_input(&version, config)
+            .map_err(|source| Error::ApplicableVersionError { source })?
+            .ok_or(Error::VersionNotFound { version })?;
+
+        #[cfg(windows)]
+        let bin_path = applicable_version.path().to_path_buf();
+
+        #[cfg(unix)]
+        let bin_path = applicable_version.path().join("bin");
+
+        let path_env = {
+            let paths_env = std::env::var_os("PATH").ok_or(Error::CantReadPathVariable)?;
+            let mut paths: Vec<_> = std::env::split_paths(&paths_env).collect();
+            paths.insert(0, bin_path);
+            std::env::join_paths(paths)
+                .map_err(|source| Error::CantAddPathToEnvironment { source })?
+        };
+
+        log::debug!("Running {} with PATH={:?}", binary, path_env);
+
+        let exit_status = Command::new(binary)
+            .args(arguments)
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .env("PATH", path_env)
+            .spawn()
+            .map_err(|source| Error::CantSpawnProgram {
+                source,
+                binary: binary.to_string(),
+            })?
+            .wait()
+            .expect("Failed to grab exit code");
+
+        let code = exit_status.code().ok_or(Error::CantReadProcessExitCode)?;
+        std::process::exit(code);
+    }
 }
