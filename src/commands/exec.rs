1667bd8ceabcdd6d0de9736b9c269e61641b0c63
berlinen <1292393108@qq.com>
2024-07-01 15:46:14 +0800

    feat: define exec Exec

--- src/commands/exec.rs
+++ src/commands/exec.rs
@@ -9,3 +9,16 @@ use crate::user_version_reader::UserVersionReader;
 use colored::Colorize;
 use std::process::{Command, Stdio};
 use thiserror::Error;
+
+#[derive(Debug, clap::Parser)]
+#[clap(trailing_var_arg = true)]
+pub struct Exec {
+    /// Either an explicit version, or a filename with the version written in it
+    #[clap(long = "using")]
+    version: Option<UserVersionReader>,
+    /// Deprecated. This is the default now.
+    #[clap(long = "using-file", hide = true)]
+    using_file: bool,
+    /// The command to run
+    arguments: Vec<String>,
+}
