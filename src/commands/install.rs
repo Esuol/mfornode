6be5ccf7594aec8779d2e673f658397f634c0cf2
berlinen <1292393108@qq.com>
2024-07-02 18:04:13 +0800

    feat: define install mod

---
+++ src/commands/install.rs
@@ -0,0 +1,14 @@
+use super::command::Command;
+use crate::alias::create_alias;
+use crate::arch::get_safe_arch;
+use crate::config::FnmConfig;
+use crate::downloader::{install_node_dist, Error as DownloaderError};
+use crate::lts::LtsType;
+use crate::outln;
+use crate::remote_node_index;
+use crate::user_version::UserVersion;
+use crate::version::Version;
+use crate::version_files::get_user_version_for_directory;
+use colored::Colorize;
+use log::debug;
+use thiserror::Error;
