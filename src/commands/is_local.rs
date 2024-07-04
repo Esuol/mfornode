0bf53a9e13e8a601bc32bb85f5ebc96247379861
berlinen <1292393108@qq.com>
2024-07-04 16:59:37 +0800

    feat: impl super::command::Command for LsLocal

--- src/commands/is_local.rs
+++ src/commands/is_local.rs
@@ -8,3 +8,41 @@ use thiserror::Error;
 
 #[derive(clap::Parser, Debug)]
 pub struct LsLocal {}
+
+impl super::command::Command for LsLocal {
+    type Error = Error;
+
+    fn apply(self, config: &FnmConfig) -> Result<(), Self::Error> {
+        let base_dir = config.installations_dir();
+        let mut versions = crate::installed_versions::list(base_dir)
+            .map_err(|source| Error::CantListLocallyInstalledVersion { source })?;
+        versions.insert(0, Version::Bypassed);
+        versions.sort();
+        let aliases_hash =
+            generate_aliases_hash(config).map_err(|source| Error::CantReadAliases { source })?;
+        let curr_version = current_version(config).ok().flatten();
+
+        for version in versions {
+            let version_aliases = match aliases_hash.get(&version.v_str()) {
+                None => String::new(),
+                Some(versions) => {
+                    let version_string = versions
+                        .iter()
+                        .map(StoredAlias::name)
+                        .collect::<Vec<_>>()
+                        .join(", ");
+                    format!(" {}", version_string.dimmed())
+                }
+            };
+
+            let version_str = format!("* {version}{version_aliases}");
+
+            if curr_version == Some(version) {
+                println!("{}", version_str.cyan());
+            } else {
+                println!("{version_str}");
+            }
+        }
+        Ok(())
+    }
+}
