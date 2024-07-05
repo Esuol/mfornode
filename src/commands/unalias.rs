cb9151553cdc1df1e5d49316d4a8a015f892df36
berlinen <1292393108@qq.com>
2024-07-05 16:01:08 +0800

    feat: unalias impl

---
+++ src/commands/unalias.rs
@@ -0,0 +1,6 @@
+use super::command::Command;
+use crate::fs::remove_symlink_dir;
+use crate::user_version::UserVersion;
+use crate::version::Version;
+use crate::{choose_version_for_user_input, config::FnmConfig};
+use thiserror::Error;
