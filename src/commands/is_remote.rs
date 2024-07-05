b2fbed4de331c74708181a53a2dc07c92e32165d
berlinen <1292393108@qq.com>
2024-07-05 15:58:19 +0800

    feat: define is_remote

--- src/commands/is_remote.rs
+++ src/commands/is_remote.rs
@@ -4,3 +4,21 @@ use thiserror::Error;
 
 #[derive(clap::Parser, Debug)]
 pub struct LsRemote {}
+
+impl super::command::Command for LsRemote {
+    type Error = Error;
+
+    fn apply(self, config: &FnmConfig) -> Result<(), Self::Error> {
+        let all_versions = remote_node_index::list(&config.node_dist_mirror)?;
+
+        for version in all_versions {
+            print!("{}", version.version);
+            if let Some(lts) = &version.lts {
+                print!(" ({lts})");
+            }
+            println!();
+        }
+
+        Ok(())
+    }
+}
