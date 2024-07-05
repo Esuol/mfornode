b07dc2abe0307d2916d1d5bea8e397852efb1f46
berlinen <1292393108@qq.com>
2024-07-05 16:01:22 +0800

    feat: define Unalias

--- src/commands/unalias.rs
+++ src/commands/unalias.rs
@@ -4,3 +4,8 @@ use crate::user_version::UserVersion;
 use crate::version::Version;
 use crate::{choose_version_for_user_input, config::FnmConfig};
 use thiserror::Error;
+
+#[derive(clap::Parser, Debug)]
+pub struct Unalias {
+    pub(crate) requested_alias: String,
+}
